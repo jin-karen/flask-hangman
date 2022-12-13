bebf2e8fcded9223c223bdb1ea396a66aeb32795
Karen Jin <102393842+jin-karen@users.noreply.github.com>
2022-12-13 18:09:23 -0500

    Add user login logout capabilities

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,19 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
+# initiate the Flask app
 app = Flask(__name__)
+# use the config.py file for configuration
 app.config.from_object(Config)
+# use SQLAlchemy for database management
 db = SQLAlchemy(app)
+# use Flask-Migrate extension for database migration management
 migrate = Migrate(app, db)
+# use Flask-Login extension for login form
+## initializes the extension
 login = LoginManager(app)
+## tells flask-login which is login view function to use protected pages
+login.login_view = 'login'
 
 from app import routes, models
+
--- app/routes.py
+++ app/routes.py
@@ -3,13 +3,15 @@ from app import app
 from app.forms import LoginForm
 from flask_login import current_user, login_user
 from app.models import User
+from flask_login import logout_user
+from flask_login import login_required
+from flask import request
+from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
 def index():
-    login = 'Karen'
-    users = [{'username':'Karen','games':'1000','won':'1000','lost':'0'}]
-    return render_template('index.html', title='Home', users=users, login=login)
+    return render_template('index.html', title='Home', login=login)
 
 @app.route('/login', methods=['GET','POST'])
 def login():
@@ -22,5 +24,18 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user,remember=form.remember_me.data)
-       	return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+       	return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/game')
+def game():
+    #incomplete
+    return render_template('base.html', title="Game Page")
