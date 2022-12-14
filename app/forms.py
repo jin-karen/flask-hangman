0d8ec238fe0fd0b3c5f56380bb83fc98980bf230
Karen Jin <102393842+jin-karen@users.noreply.github.com>
2022-12-13 19:30:51 -0500

    edit forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,9 @@
-#Login form 
-#Asks for username, password, "remember me" check box, and a submit button
-
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
+#Login form 
+#Asks for username, password, "remember me" check box, and a submit button
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
